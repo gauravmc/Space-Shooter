2e88a8057bdd500f32553c02faf66d716b980121
Gaurav Chande <gmail@gauravchande.com>
2017-11-21 01:39:00 +0530

    1. Shader background with sprite overlay
    2. Add Player with its movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    [SerializeField] private float speed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+        transform.position = new Vector3(0, -3, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Movement();
+    }
+
+    private void Movement() {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
+        transform.Translate(Vector3.up * speed * verticalInput * Time.deltaTime);
+
+        if (transform.position.y > 0) {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y < -4.5f) {
+            transform.position = new Vector3(transform.position.x, -4.5f, 0);
+        }
+
+        if (transform.position.x > 9.5f) {
+            transform.position = new Vector3(-9.5f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -9.5f) {
+            transform.position = new Vector3(9.5f, transform.position.y, 0);
+        }
+    }
+}
