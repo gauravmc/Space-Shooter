7249c2b59f38ce0db7bab31f586fb6724c2df7f1
Gaurav Chande <gmail@gauravchande.com>
2017-11-21 14:55:00 +0530

    Make power up a bit slower

--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Powerup : MonoBehaviour {
-    [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float speed = 3.0f;
 
     // Use this for initialization
     void Start()
